36bc596ca602f7791bed155f1e70bd418c6fafad
Oleg Marko <olegmarko7@gmail.com>
2019-04-15 23:37:08 +0300

    Updated

--- resources/views/site/car-tour.blade.php
+++ resources/views/site/car-tour.blade.php
@@ -1,12 +1,29 @@
 @extends('layouts.site')
 
 @section('content')
-    <section class="section_about">
-        <div class="container_about">
-            <h1 class="title">Джипінг тур</h1>
+    <section class="section_galery">
+        <div class="slider-container">
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c1.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c2.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c3.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c4.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c5.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c6.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c7.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c8.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c9.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c10.jpg') }});"></div>
+            <div class="slider-container-i" style="background-image: url({{ asset('assets/images/c11.jpg') }});"></div>
         </div>
-        <div class="about_qw">
-            <div class="qw_content">
+
+        <div class="gallery_buttons_position">
+            <div class="gallery_buttons_wrap">
+                <button class="gallery_slider_next slick-arrow" style="display: block;">
+                    <i class="i_mini_arrow" style="background-image: url({{ asset('assets/svg/arrow.svg') }});"></i>
+                </button>
+                <button class="gallery_slider_prev slick-arrow" style="display: block;">
+                    <i class="i_mini_arrow" style="background-image: url({{ asset('assets/svg/arrow.svg') }});"></i>
+                </button>
             </div>
         </div>
     </section>
--- resources/views/site/store.blade.php
+++ resources/views/site/store.blade.php
@@ -7,26 +7,21 @@
         </div>
         <div class="about_qw">
             <div class="store">
-                @foreach([1, 2, 3, 4, 5, 6] as $item)
+                @foreach([1, 2, 3, 4] as $item)
                 <div class="g-i-tile g-i-tile-catalog">
                     <div class="over-wraper">
                         <div class="g-i-tile-i available">
                             <div class="g-i-tile-i-box">
                                 <div class="g-i-tile-i-box-desc">
                                     <div class="clearfix pos-fix">
-                                        <div class="g-i-tile-i-image fix-height" tool_tip="1" id="image_item28720489">
-                                            <div class="cat-g-id-container">
-                                                <div class="g-id-wrap clearfix">
-                                                    <div class="g-id" style="display: none;">28720489</div>
-                                                </div>
-                                            </div>
-                                            <a href="https://rozetka.com.ua/luminarc_n5277/p28720489/" class="responsive-img centering-child-img" onclick="document.fireEvent('goodsImageClick', {extend_event: [{name: 'goods_id', value: 28720489}]}); return true">
-                                                <img src="https://i2.rozetka.ua/goods/2436892/luminarc_n5277_images_2436892233.jpg" width="240" height="128" alt="Детский набор Luminarc Disney Frozen Winter Magic из 3 предметов (N5277)" title="Детский набор Luminarc Disney Frozen Winter Magic из 3 предметов (N5277)">
+                                        <div class="g-i-tile-i-image fix-height">
+                                            <a href="#" class="responsive-img centering-child-img">
+                                                <img src="{{ asset('/assets/images/product.png') }}" alt="product" title="product">
                                             </a>
                                         </div>
                                     </div>
                                     <div class="g-i-tile-i-title clearfix">
-                                        <a href="https://rozetka.com.ua/luminarc_n5277/p28720489/" onclick="document.fireEvent('goodsTitleClick', {extend_event: [{name: 'goods_id', value: 28720489}, {name: 'eventLocation', value: 'CatalogHighLevel'}]}); return true">
+                                        <a href="#">
                                             Детский набор Luminarc Disney Frozen Winter Magic из 3 предметов (N5277)
                                         </a>
                                     </div>
