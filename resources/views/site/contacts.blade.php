8ef844937247d0961557bfd4b5a01436e79e37fa
Oleg Marko <olegmarko7@gmail.com>
2019-06-23 12:12:58 +0300

    Updated map

--- resources/views/site/contacts.blade.php
+++ resources/views/site/contacts.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="map-container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2650.2176565212667!2d24.587428!3d48.375555!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x846fae244881c4ae!2sMini+Pyvovarnya+&#39;&#39;hirs%CA%B9ke&#39;&#39;!5e0!3m2!1sru!2sua!4v1541614099925"
-                width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1324.961744541596!2d24.590436!3d48.381206!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c87cdfaa48b7a99!2z0JPQuNGA0YzRgdC60LUg0L_QuNCy0L7QstCw0YDQvdGP!5e0!3m2!1sru!2sua!4v1561280885032!5m2!1sru!2sua"
+            width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
     </section>
 @endsection
