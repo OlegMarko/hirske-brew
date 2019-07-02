d88f9393253686f9bfb7e62fcb295626ae3d64e0
Oleg Marko <olegmarko7@gmail.com>
2019-07-02 15:56:35 +0300

    Fix map

--- resources/views/site/contacts.blade.php
+++ resources/views/site/contacts.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="map-container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1324.961744541596!2d24.590436!3d48.381206!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c87cdfaa48b7a99!2z0JPQuNGA0YzRgdC60LUg0L_QuNCy0L7QstCw0YDQvdGP!5e0!3m2!1sru!2sua!4v1561280885032!5m2!1sru!2sua"
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1324.9647378797192!2d24.590005!3d48.381091!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbf69ea7811242750!2zUHl2b3Zhcm55YSAiSGlyc8q5a2Ui!5e0!3m2!1sru!2sua!4v1562072069330!5m2!1sru!2sua"
             width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
     </section>
 @endsection
